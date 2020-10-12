7880bf1046ce5734a67dbd4511c758da831eb3cf
sim <simworld@tuta.io>
2020-10-12 23:49:51 +0100

    new instance of objects created

--- src/tracker/Test.java
+++ src/tracker/Test.java
@@ -1,23 +1,59 @@
 package tracker;
 
+import java.text.NumberFormat;
 import java.util.*;
 import java.io.*;
 
 public class Test {
-    public static void main(String[] args){
 
+
+    public static void main(String[] args) {
+        Repository repo = Repository.getRepository();
         Scanner scanner = new Scanner(System.in);
-        Category cat1 = new Category("Travel");
-        Expense ex1 = new Expense(cat1.getCategoryId(), cat1.getName(), 3.3);
+        //Category cat = new Category("Travel");
+        //Expense ex = new Expense(cat.getCategoryId(), cat.getName(), 3.3);
         //System.out.println(cat1.getName());
 //        System.out.println(ex1.getAmount());
 //        System.out.println(ex1.getName());
+        System.out.println("Enter a Category:");
+        Category cat = new Category(scanner.next());
+//        System.out.println("Category inserted is: " + cat.getName() );
+        repo.getCatList().add(cat);
+        List<Category> clist = repo.catList;
+        for (Category cliste : clist) {
+            System.out.println(cliste.getName());
+        }
 
-        Repository lista = new Repository(cat1, ex1);
-//        lista.add(cat1);
-//        for(Repository list : lista){
-//            System.out.println(list.getName());
-//        }
-        System.out.println(lista.toString());
+        System.out.println("Enter an Expense:");
+        System.out.println("Type the Category:");
+        String cat1 = scanner.next();
+        System.out.println("Type the Amount");
+        Double amt = scanner.nextDouble();
+        Expense exp = new Expense(cat.getCategoryId(), cat.getName(), amt);
+        repo.getExpenseList().add(exp);
+        List<Expense> elist = repo.expenseList;
+        for (Expense eliste : elist){
+            System.out.println("Category:"+ eliste.getName() + "\n" + "Amount:" + eliste.getAmount() + "\n" + "Category ID:" + eliste.getCategoryId() + "\n" + "Expense ID:" + eliste.getExpenseId());
+        }
+//        Repository lista = new Repository(cat, ex);
+//        repo.getCatList().add(cat);
+//        repo.getExpenseList().add(ex);
+
+//        System.out.println("Insert a Category:\n");
+//        String cat = scanner.next();
+//        repo.catList.add(cat);
+//
+
+
+//        String exp = scanner.nextLine();
+//        repo.getExpenseList().add(exp);
+
+////        lista.add(cat1);
+////        for(Repository list : lista){
+////            System.out.println(list.getName());
+////        }
+//
+//        System.out.println(repo.getCatList());
     }
+
 }
